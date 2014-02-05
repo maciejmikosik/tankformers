320625da44179fdf9794d4603f9696f00cc235fd
Maciej Mikosik <maciej@mikosik.com>
2014-02-05 20:58:33 +0100

    configured eclipse settings

--- tankformers-desktop/src/com/tankformers/Main.java
+++ tankformers-desktop/src/com/tankformers/Main.java
@@ -4,13 +4,13 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 public class Main {
-	public static void main(String[] args) {
-		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-		cfg.title = "tankformers";
-		cfg.useGL20 = false;
-		cfg.width = 480;
-		cfg.height = 320;
-		
-		new LwjglApplication(new Tankformers(), cfg);
-	}
+  public static void main(String[] args) {
+    LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+    cfg.title = "tankformers";
+    cfg.useGL20 = false;
+    cfg.width = 480;
+    cfg.height = 320;
+
+    new LwjglApplication(new Tankformers(), cfg);
+  }
 }
--- tankformers-html/src/com/tankformers/client/GwtLauncher.java
+++ tankformers-html/src/com/tankformers/client/GwtLauncher.java
@@ -1,19 +1,19 @@
 package com.tankformers.client;
 
-import com.tankformers.Tankformers;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.tankformers.Tankformers;
 
 public class GwtLauncher extends GwtApplication {
-	@Override
-	public GwtApplicationConfiguration getConfig () {
-		GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(480, 320);
-		return cfg;
-	}
+  @Override
+  public GwtApplicationConfiguration getConfig() {
+    GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(480, 320);
+    return cfg;
+  }
 
-	@Override
-	public ApplicationListener getApplicationListener () {
-		return new Tankformers();
-	}
+  @Override
+  public ApplicationListener getApplicationListener() {
+    return new Tankformers();
+  }
 }
