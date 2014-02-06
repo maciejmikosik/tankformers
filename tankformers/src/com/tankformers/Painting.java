db71142e4d80bfc82ee27364ee41f13362b97718
Maciej Mikosik <maciej@mikosik.com>
2014-02-06 13:32:29 +0100

    refactored Painting

--- tankformers/src/com/tankformers/Painting.java
+++ tankformers/src/com/tankformers/Painting.java
@@ -9,27 +9,36 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.tankformers.model.Tank;
 
 public class Painting {
-  private static Texture texture;
-  private static Sprite sprite;
+  private static Texture tankTexture;
+  private static Sprite tankSprite;
 
   static {
-    texture = new Texture(Gdx.files.internal("data/tank_green_64.png"));
-    texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+    tankTexture = tankTexture("data/tank_green_64.png");
+    tankSprite = tankSprite(tankTexture);
+  }
 
-    sprite = new Sprite(new TextureRegion(texture, 0, 0, 64, 64));
+  private static Sprite tankSprite(Texture texture) {
+    Sprite sprite = new Sprite(new TextureRegion(texture, 0, 0, 64, 64));
     sprite.setSize(0.05f, 0.05f * sprite.getHeight() / sprite.getWidth());
     sprite.setOrigin(sprite.getWidth() / 2, sprite.getHeight() / 2);
     sprite.setPosition(-sprite.getWidth() / 2, -sprite.getHeight() / 2);
+    return sprite;
+  }
+
+  private static Texture tankTexture(String fileName) {
+    Texture texture = new Texture(Gdx.files.internal(fileName));
+    texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+    return texture;
   }
 
   public static void dispose() {
-    texture.dispose();
+    tankTexture.dispose();
   }
 
   public static void draw(Tank tank, SpriteBatch batch) {
-    sprite.setRotation(tank.direction);
-    sprite.setPosition(tank.position.x, tank.position.y);
-    sprite.draw(batch);
+    tankSprite.setRotation(tank.direction);
+    tankSprite.setPosition(tank.position.x, tank.position.y);
+    tankSprite.draw(batch);
   }
 
 }
