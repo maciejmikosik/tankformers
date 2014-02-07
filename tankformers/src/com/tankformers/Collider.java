aa13d9df1d4571fd51e8bf5d36070733e02597d2
Maciej Mikosik <maciej@mikosik.com>
2014-02-07 19:25:05 +0100

    implemented tank-tank collision

--- tankformers/src/com/tankformers/Collider.java
+++ tankformers/src/com/tankformers/Collider.java
@@ -1,16 +1,45 @@
 package com.tankformers;
 
+import static com.tankformers.math.Calculations.add;
 import static com.tankformers.math.Calculations.distance;
+import static com.tankformers.math.Calculations.length;
+import static com.tankformers.math.Calculations.minus;
+import static com.tankformers.math.Calculations.multiply;
+import static com.tankformers.math.Calculations.unit;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import com.tankformers.math.Vector;
 import com.tankformers.model.Bullet;
 import com.tankformers.model.Ground;
 import com.tankformers.model.Tank;
 
 public class Collider {
   public static void solveCollision(Ground ground) {
+    solveTankWithBullet(ground);
+    solveTankWithTank(ground);
+  }
+
+  private static void solveTankWithTank(Ground ground) {
+    List<Tank> tanks = ground.tanks;
+    for (int i = 0; i < tanks.size(); i++) {
+      for (int j = i + 1; j < tanks.size(); j++) {
+        Tank tankA = tanks.get(i);
+        Tank tankB = tanks.get(j);
+        Vector vector = add(tankB.position, minus(tankA.position));
+
+        float space = length(vector) - 0.4f * Tank.size * 2;
+        if (space < 0f) {
+          Vector correction = multiply(0.5f * -space, unit(vector));
+          tankA.position = add(tankA.position, minus(correction));
+          tankB.position = add(tankB.position, correction);
+        }
+      }
+    }
+  }
+
+  private static void solveTankWithBullet(Ground ground) {
     List<Bullet> hits = new ArrayList<Bullet>();
     for (Bullet bullet : ground.bullets) {
       boolean areColliding = areColliding(bullet, ground.tanks.get(0))
