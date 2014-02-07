0f1c0a90662d347a952ab80521976c417f0fe060
Maciej Mikosik <maciej@mikosik.com>
2014-02-07 22:18:04 +0100

    filled game ground with walls

--- tankformers/src/com/tankformers/Setup.java
+++ tankformers/src/com/tankformers/Setup.java
@@ -1,5 +1,14 @@
 package com.tankformers;
 
+import static com.tankformers.math.Calculations.add;
+import static com.tankformers.math.Calculations.multiply;
+import static com.tankformers.math.Calculations.vectorPolar;
+import static com.tankformers.math.Vector.vector;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.tankformers.math.Vector;
 import com.tankformers.model.Ground;
 import com.tankformers.model.Tank;
 import com.tankformers.model.Wall;
@@ -20,11 +29,21 @@ public class Setup {
     tank.direction = 290;
     ground.tanks.add(tank);
 
-    Wall wall = new Wall();
-    wall.position.x = -0.2f;
-    wall.position.y = 0.1f;
+    ground.walls.addAll(walls(10, vector(-0.3f, 0.3f), 290));
+    ground.walls.addAll(walls(20, vector(-0.1f, 0.15f), 280));
+    ground.walls.addAll(walls(10, vector(0.1f, -0.1f), 0));
 
-    ground.walls.add(wall);
     return ground;
+  }
+
+  private static List<Wall> walls(int number, Vector position, float angle) {
+    List<Wall> walls = new ArrayList<Wall>();
+    Vector diff = vectorPolar(angle, Wall.size);
+    for (int i = 0; i < number; i++) {
+      Wall wall = new Wall();
+      wall.position = add(position, multiply(i, diff));
+      walls.add(wall);
+    }
+    return walls;
   }
 }
